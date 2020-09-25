a923298b52fcb414ba6767d469630d0aa510d2d4
karankataria-kk <kkataria682@gmail.com>
2020-09-25 23:18:22 +0530

    Framework Set-up

---
+++ src/main/java/Common/LogProcess.java
@@ -0,0 +1,5 @@
+package Common;
+import org.apache.logging.log4j.Logger;
+public class LogProcess {
+	//static Logger log = new Logger(); 
+}
---
+++ src/main/java/Synchronisation/Synchronise.java
@@ -0,0 +1,6 @@
+package Synchronisation;
+public class Synchronise {
+	public static boolean isDisplayed() {
+		return true;
+	}
+}
