9a9acaede5117ae39acc5a991e01c9098ab54b60
karankataria-kk <kkataria682@gmail.com>
2020-09-30 01:37:00 +0530

    No certai major changes

--- src/main/java/LogHandler/Log.java
+++ src/main/java/LogHandler/Log.java
@@ -1,21 +1,18 @@
 package LogHandler;
 
 import org.apache.log4j.Logger;
-import java.io.File;
-import org.apache.log4j.Logger;
-import org.apache.log4j.xml.DOMConfigurator;
 public class Log {
-	final Logger logger = Logger.getLogger(Log.class);
-	public void info(String message){
+	final static Logger logger = Logger.getLogger(Log.class);
+	public static void info(String message){
 		logger.info(message);			
 	}
-	public void warn(String message) {
+	public static void warn(String message) {
 		logger.warn(message);
 	}
-	public void error(String message) {
+	public static void error(String message) {
 		logger.error(message);
 	}
-	public void fatal(String message) {
+	public static void fatal(String message) {
 		logger.fatal(message);
 	}
 	public void debug(String message) {
