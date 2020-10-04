836f328f102bcb61e6bb86e7d3845826a4cd8eb9
karankataria-kk <kkataria682@gmail.com>
2020-10-04 18:04:59 +0530

    TestNG files updated and added

---
+++ src/test/Suites/TestSuite1/TestSuite.java
@@ -0,0 +1,31 @@
+public class TestSuite {
+	@BeforeClass
+	void beforeClass() {
+		/*
+		 * Enter the functions you need  as per your test run
+		 */
+	}
+	@BeforeTest
+	void beforeTest() {
+		/*
+		 * Setup the configurations and test enviorment
+		 */
+	}
+	@Test(prority = 1)
+	void useCase1() {
+		/*
+		 * Use case ass ons
+		 */
+	}
+	@AfterTest
+	void afterTest() {
+		/*
+		 * Enter executions you needto run aftertest  completion
+		 */
+	}
+	void afterClass() {
+		/*
+		 * Enter the usecase endings as per the reports generated and outputs
+		 */
+	}
+}
